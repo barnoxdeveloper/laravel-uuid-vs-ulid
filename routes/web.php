ade8405eb8506d3b927261ba779951bdceb5bd49
barnoxdeveloper <barnoxdev@gmail.com>
2023-07-04 22:14:08 +0700

    Update 04 Juli 2023 - First Commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\{Route, Auth};
+use App\Http\Controllers\Auth\VerificationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware(['auth', 'verified']);;
+
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+
+
+Route::post('/email/verification-notification', [VerificationController::class, 'send'])
+     ->middleware(['auth', 'throttle:6,1'])
+     ->name('verification.resend');
+
+Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])
+     ->middleware(['auth', 'signed', 'throttle:6,1'])
+     ->name('verification.verify');
+
+Auth::routes();
