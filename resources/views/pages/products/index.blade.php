255d700015aac01d3137b7095c0bd1e9752aac02
tunasmitra <tunasmitra.id@gmail.com>
2024-07-05 15:25:35 +0700

    Feat: update products page

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Products Page</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     </head>
     <body>
         <div class="container p-3">
@@ -23,37 +22,67 @@
                             </thead>
                             <tbody></tbody>
                         </table>
+                        <div class="d-flex justify-content-end">
+                            <div class="pagination"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
         <script>
             $(document).ready(function () {
-                $("#table").DataTable({
-                    processing: true,
-                    serverSide: true,
-                    paging: true,
-                    ajax: {
+                // Initial load of first page
+                loadTable(1);
+
+                // Pagination link click handler
+                $(document).on('click', '.pagination a', function(e) {
+                    e.preventDefault();
+                    const page = $(this).attr('href').split('page=')[1];
+                    loadTable(page);
+                });
+
+                function loadTable(page) {
+                    $.ajax({
                         url: "{{ route('api-product') }}",
                         type: "GET",
-                        dataSrc: function (response) {
-                            console.info(response);
-                            return response.data;
+                        data: { page: page },
+                        success: function (response) {
+                            var tbody = $("#table tbody");
+                            tbody.empty();
+                            
+                            $.each(response.data, function (index, product) {
+                                var row = "<tr>";
+                                row += "<td>" + ((page - 1) * 10 + index + 1) + "</td>";
+                                row += "<td>" + product.name + "</td>";
+                                row += "<td>" + product.price + "</td>";
+                                row += "<td>" + product.quantity + "</td>";
+                                row += "</tr>";
+                                tbody.append(row);
+                            });
+
+                            var paginationHtml = '';
+                            $.each(response.links, function(index, link) {
+                                if (link.url === null) {
+                                    paginationHtml += '<span class="page-link">' + link.label + '</span>';
+                                } else {
+                                    var activeClass = link.active ? 'active' : '';
+                                    paginationHtml += '<li class="page-item ' + activeClass + '"><a class="page-link" href="' + link.url + '">' + link.label + '</a></li>';
+                                }
+                            });
+                            $('.pagination').html('<ul class="pagination">' + paginationHtml + '</ul>');
                         },
                         error: function (xhr, error, thrown) {
                             console.error(xhr, error, thrown);
                         },
-                    },
-                    columns: [
-                        { title: "#", data: "id" },
-                        { title: "Name", data: "name" },
-                        { title: "Price", data: "price" },
-                        { title: "Qty", data: "quantity" },
-                    ],
+                    });
+                }
+
+                $(document).on('click', '.pagination a', function(e) {
+                    e.preventDefault();
+                    const page = $(this).attr('href').split('page=')[1];
+                    loadTable(page);
                 });
             });
         </script>
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-// Route::get('/api-product', [ProductController::class, 'index'])->name('api-product')->middleware(['auth', 'verified']);
+Route::get('/api-product', [ProductController::class, 'index'])->name('api-product')->middleware(['auth', 'verified']);
