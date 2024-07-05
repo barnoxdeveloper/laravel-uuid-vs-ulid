2b9ae400b696b72897907128e2865aebee9a8c00
tunasmitra <tunasmitra.id@gmail.com>
2024-07-05 15:50:49 +0700

    Update: Api Products

--- app/Http/Controllers/API/V1/ProductController.php
+++ app/Http/Controllers/API/V1/ProductController.php
@@ -13,7 +13,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate(10);
+        // $products = Product::paginate(10);
+        $products = Product::all();
         return response()->json($products);
     }
 
